bd6382e99b7512a210dd217dca082ecddc2414b8
OliuOliuHiu <hieuhd25022004@ggmail.com>
2025-02-13 21:30:58 +0700

    add function Manage Product

--- Salemanagement/ui/MainprogramMainWindowExt.py
+++ Salemanagement/ui/MainprogramMainWindowExt.py
@@ -1,5 +1,10 @@
 import sys
+
+from PyQt6.QtWidgets import QMainWindow
+
 from Salemanagement.ui.MainProgramMainWindow import Ui_MainWindow
+from Salemanagement.ui.ProductMainWindowExt import ProductMainWindowExt
+
 
 class MainProgramMainWindowExt(Ui_MainWindow):
     def setupUi(self, MainWindow):  # Fix: Change "SetupUi" to "setupUi"
@@ -11,5 +16,13 @@ class MainProgramMainWindowExt(Ui_MainWindow):
 
     def setupSignalAndSlot(self):
         self.actionthoatphanmem.triggered.connect(self.xuly_thoat)
+        self.actionQuanLySanPhamDanhMuc.triggered.connect(self.xuly_momanhinh_qlspdm)
+
     def xuly_thoat(self):
         sys.exit(0)
+
+    def xuly_momanhinh_qlspdm(self):
+        self.mainwindow = QMainWindow()
+        self.myui = ProductMainWindowExt()
+        self.myui.setupUi(self.mainwindow)
+        self.myui.showWindow()
